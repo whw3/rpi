83dc4cd0309adee908bcbbc93657018956b023d8
W.Welch <whw3@users.noreply.github.com>
2017-09-03 21:24:10 -0500

    Update 01-docker-entrypoint.sh
    damn auto-correct

--- 01-docker-entrypoint.sh
+++ 01-docker-entrypoint.sh
@@ -3,6 +3,6 @@ if [[ -x /usr/local/bin/docker-entrypoint ]]; then
     exec /usr/local/bin/docker-entrypoint
 elif [[ -x /docker-entrypoint ]]; then
     exec /docker-entrypoint
-else [[ -x /entrypoint.sh ]]; then
+elif [[ -x /entrypoint.sh ]]; then
     exec /entrypoint.sh
 fi
